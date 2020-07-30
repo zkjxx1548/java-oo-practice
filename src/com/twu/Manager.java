fccd4115ca0314d493d5078eac556aca75366e13
zkj <438101845@qq.com>
2020-07-30 09:22:22 +0800

    [zkj]-feat: finish Manager.class-viewEvent.method

--- src/com/twu/Manager.java
+++ src/com/twu/Manager.java
@@ -1,4 +1,17 @@
 package com.twu;
 
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Manager {
+    public void viewEvent(List<Event> events) {
+        AtomicInteger i = new AtomicInteger(1);
+        events.stream()
+                .sorted((o1, o2) -> o2.getHeat() - o1.getHeat())
+                .forEach(event -> {
+                    System.out.println(String.format("排名：%d，描述：%s，热度：%d。",
+                            i.getAndIncrement(), event.getDescribe(), event.getHeat()));
+                    event.setRanking(i.get() - 1);
+                });
+    }
 }
