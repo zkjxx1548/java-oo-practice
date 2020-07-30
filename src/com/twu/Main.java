d5a876814b145d8b7681a3ef24dbfc697e4be72c
zkj <438101845@qq.com>
2020-07-30 09:16:49 +0800

    [zkj]-feat: finish homePage.method

--- src/com/twu/Main.java
+++ src/com/twu/Main.java
@@ -2,6 +2,7 @@ package com.twu;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
@@ -21,5 +22,22 @@ public class Main {
 
         Manager manager = new Manager();
         User user = new User();
+    }
+
+    public static void homePage(Manager manager, User user, List<Event> events) {
+        System.out.println("请选择权限：");
+        System.out.println("1.管理员");
+        System.out.println("2.普通用户");
+        Scanner scanner = new Scanner(System.in);
+        int choose = scanner.nextInt();
+        switch (choose) {
+            case 1:
+                managerPage(manager, user, events);
+                break;
+            case 2:
+                userPage(manager, user, events);
+                break;
+        }
+
     }
 }
